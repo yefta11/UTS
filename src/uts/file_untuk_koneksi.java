60fa16d34d6ed52411c1ad354560754a2da8ffbc
yefta <yefta>
2017-03-16 22:43:15 +0700

    commit1

---
+++ src/uts/file_untuk_koneksi.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author kambing.ui.ac.id
+ */
+public class file_untuk_koneksi { 
+ 
+    private static Connection koneksi;
+    
+    public static Connection GetConnection() throws SQLException 
+    {         
+        if (koneksi == null) 
+        {             
+            Driver driver = new Driver();             
+            koneksi = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/db_uts?zeroDateTimeBehavior=convertToNull", "root", "");         
+        }         
+        return koneksi;     
+    }      
+}
